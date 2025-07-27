d025595c133f9c60d8c07f0b2dfcad44ed2ad628
SamanwoySaha <samanwoy.web@gmail.com>
2025-07-27 16:03:26 +0600

    added user settings

--- app/(main)/(routes)/documents/page.tsx
+++ app/(main)/(routes)/documents/page.tsx
@@ -1,8 +1,37 @@
+"use client"
+import Image from 'next/image'
 import React from 'react'
+import { useUser } from "@clerk/clerk-react"
+import { PlusCircle } from 'lucide-react'
+import { Button } from '@/components/ui/button'
 
 const Documents = () => {
+  const { user } = useUser();
+
   return (
-    <div>Documents</div>
+    <div className='h-full flex flex-col items-center justify-center space-y-4'>
+      <Image
+        src="/empty.png"
+        height="300"
+        width="300"
+        alt="Empty"
+        className='dark:hidden'
+      />
+      <Image
+        src="/empty-dark.png"
+        height="300"
+        width="300"
+        alt="Empty"
+        className='hidden dark:block'
+      />
+      <h2 className='text-lg font-medium'>
+        {user?.firstName}&apos; poetry
+      </h2>
+      <Button>
+        <PlusCircle className='h-4 w-4 mr-2' />
+        Create a note
+      </Button>
+    </div>
   )
 }
 
